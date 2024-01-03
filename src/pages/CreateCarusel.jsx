7c27c487a4da306b2a95e8cf2c3c929585bf0085
arefin5 <arefintalukder5@gmail.com>
2024-01-03 15:39:04 +0600

    fix issue

--- src/pages/CreateCarusel.jsx
+++ src/pages/CreateCarusel.jsx
@@ -1,4 +1,3 @@
-"use client"
 import React, { useEffect, useState } from 'react';
 import CreateCaruselTop from '../component/CreateCaruselTop';
 import CaruselPending from '../component/pendingCarusel.jsx';
