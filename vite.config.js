18596168df8e4042baa8b90e5526ce5f0e29604b
arefin5 <arefintalukder5@gmail.com>
2024-01-03 15:34:58 +0600

    fix issue

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false,
+    sourcemap: true,
   },
   resolve: {
     alias: {
