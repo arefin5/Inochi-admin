23e508ac6651be0c6333cc2271139e4ce506069b
arefin5 <arefintalukder5@gmail.com>
2024-01-09 15:12:10 +0600

    add server

---
+++ ecosystem.config.mjs
@@ -0,0 +1,14 @@
+export default {
+    apps: [
+        {
+            name: 'vite-app',
+            script: 'serve',
+            env: {
+                PM2_SERVE_PATH: './dist',
+                PM2_SERVE_PORT: 5173,
+                PM2_SERVE_SPA: 'true',
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+};
